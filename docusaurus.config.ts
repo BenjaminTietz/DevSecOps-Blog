45b0c36972403e8d0dfbb975f5bee0e825473f11
Benjamin Tietz <benjamin0tietz@gmail.com>
2025-02-14 12:25:50 +0100

    fix: adjust baseUrl config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -30,7 +30,7 @@ const config: Config = {
   url: "https://BenjaminTietz.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/dev-blog-template/",
+  baseUrl: "/DevSecOps-Blog/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
